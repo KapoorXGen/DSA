53a371bf1e6ff5b6278e71adc3ed4095066355f6
KapoorXGen <omkapoor921@gmail.com>
2025-08-22 10:52:23 +0530

    Updated the code

--- Medium/540-single-element-in-a-sorted-array/540_single_element_in_a_sorted_array.cpp
+++ Medium/540-single-element-in-a-sorted-array/540_single_element_in_a_sorted_array.cpp
@@ -16,17 +16,12 @@ int singleNonDuplicate(vector<int>& nums) {
         if (nums[mid - 1] != nums[mid] && nums[mid] != nums[mid + 1]) return nums[mid];
 
         if (mid % 2 == 0) {
-            if (nums[mid] == nums[mid + 1]) {
-                start = mid + 2;
-            } else {
-                end = mid;
-            }
-        } else {
-            if (nums[mid] == nums[mid - 1]) {
-                start = mid + 1;
-            } else {
-                end = mid - 1;
-            }
+            if (nums[mid] == nums[mid + 1]) start = mid + 2;
+            else end = mid;
+        } 
+        else {
+            if (nums[mid] == nums[mid - 1]) start = mid + 1;
+            else end = mid - 1;
         }
     }
     return nums[start];
