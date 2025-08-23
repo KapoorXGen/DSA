8f1c09fb8232d72e0903e9efd6336fb977932743
KapoorXGen <omkapoor921@gmail.com>
2025-08-23 14:49:59 +0530

    Adding one more solved problem

---
+++ Easy/69-Sqrt(x)/69_sqrt(x).cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int mySqrt(int x) {
+    if (x == 0 || x == 1) return x;
+
+    int ans = 0;
+    int start = 0, end = x;
+
+    while (start <= end) {
+        long long mid = start + (end - start) / 2;
+        long long sq = mid * mid;
+
+        if (sq == x)
+            return mid;
+        else if (sq < x) {
+            ans = mid;
+            start = mid + 1;
+        } else {
+            end = mid - 1;
+        }
+    }
+    return ans;
+}
+
+int main() {
+    int x;
+    cout << "Enter a number: ";
+    cin >> x;
+    cout << "Square root of " << x << " is " << mySqrt(x) << endl;
+    return 0;
+}
