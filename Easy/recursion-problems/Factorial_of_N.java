39ad536cc95afc38676ca1a6eea1e14572e8296a
KapoorXGen <omkapoor921@gmail.com>
2025-09-26 09:29:14 +0530

    Updated files

---
+++ Easy/recursion-problems/Factorial_of_N.java
@@ -0,0 +1,13 @@
+class Factorial_of_N {
+    public static int fact(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        return n * fact(n - 1);
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("Factorial of " + 5 + " is " + fact(5));
+    }
+}
--- Easy/recursion-problems/Sum_of_N.java
+++ Easy/recursion-problems/Sum_of_N.java
@@ -1,4 +1,3 @@
-import java.util.Scanner;
 
 public class Sum_of_N {
 
@@ -10,10 +9,6 @@ public class Sum_of_N {
     }
 
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Enter a number: ");
-        int n = input.nextInt();
-
-        System.out.print("Sum of " + n + " is " + fun(n));
+        System.out.print("Sum of " + 5 + " is " + fun(5));
     }
 }
